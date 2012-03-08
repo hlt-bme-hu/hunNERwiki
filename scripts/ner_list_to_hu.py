c319f5c8b3cdfb82f25b6e550859be3788df6ae7
David Nemeskey <ndavid@info.ilab.sztaki.hu>
2012-03-08 17:23:29 +0100

    Keep option added to ner_list_to_hu.py

--- scripts/ner_list_to_hu.py
+++ scripts/ner_list_to_hu.py
@@ -1,6 +1,7 @@
 """Converts the NER list to Hungarian (DBpedia uses English URIs)."""
 
 import sys
+from optparse import OptionParser
 
 def read_interlang_map(interlang_file):
     """Reads the English -> Hungarian article mapping."""
@@ -17,22 +18,30 @@ def read_interlang_map(interlang_file):
     gc.enable()
     return ret
 
-def convert_ner_lang(ner_file, lang_mapping):
+def convert_ner_lang(ner_file, lang_mapping, keep=False):
     """Converts the NER list according to @p lang_mapping."""
     with open(ner_file, 'r') as infile:
         for line in infile:
             try:
                 page, cat = line.strip().split("\t")
+                if keep:
+                    print "{0}\t{1}".format(page, cat)
                 hu_page = lang_mapping[page]
                 print "{0}\t{1}".format(hu_page, cat)
             except KeyError:
-                sys.stderr.write("Unknown page " + page)
+                sys.stderr.write("Unknown page " + page + "\n")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.stderr.write("Usage: {0} ner_list language_mapping\n\n".format(__file__))
+    option_parser = OptionParser()
+    option_parser.add_option("-k", "--keep", dest="keep",
+            action="store_true", default=False,
+            help="keeps the English entries")
+    options, args = option_parser.parse_args()
+
+    if len(args) != 2:
+        sys.stderr.write("Usage: {0} [options] ner_list language_mapping\n\n".format(__file__))
         sys.exit()
 
-    lang_mapping = read_interlang_map(sys.argv[2])
-    convert_ner_lang(sys.argv[1], lang_mapping)
+    lang_mapping = read_interlang_map(args[1])
+    convert_ner_lang(args[0], lang_mapping, options.keep)
 
