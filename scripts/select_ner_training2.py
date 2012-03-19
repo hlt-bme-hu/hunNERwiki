c4f41c1f62524e93baa094eb18e4fe9056d71e97
David Nemeskey <ndavid@info.ilab.sztaki.hu>
2012-03-19 17:58:22 +0100

    Filtering added to select_ner_training2.py.

--- scripts/select_ner_training2.py
+++ scripts/select_ner_training2.py
@@ -16,6 +16,7 @@ import langtools.utils.cmd_utils
 from langtools.utils.cascading_config import CascadingConfigParser
 from langtools.utils.useful import all_partitions
 from langtools.utils.language_config import LanguageTools
+from langtools.utils import misc
 
 # Functionality needed:
 # - from Nothman et al, 2008 (and our ideas, which they blantantly stole in
@@ -152,7 +153,7 @@ class NERTrainingCallback(DefaultConllCallback):
 
     LINK_PATTERN = re.compile('^(?:w[:])(?:[a-zA-Z][a-z][A-Z]:)?(.+)$')
     
-    def __init__(self, trie, keep_discarded=False, outs=None):
+    def __init__(self, trie, keep_discarded=False, keep_filtered=False, outs=None):
         """
         Initializes the callback. If specified, the training sentences are
         written to @c outs; otherwise they are written to a separate output
@@ -171,10 +172,15 @@ class NERTrainingCallback(DefaultConllCallback):
         self._redirect_map = {}  # page: redirects
         self._trie = trie        # the NERs mentioned on the page
         self._mode = NERTrainingCallback.NO_LINK  # state machine state
+        self._punct = set(u'.?!:')  # Sentences should end with one of these
         self._keep_discarded = keep_discarded
+        self._keep_filtered = keep_filtered
 
         self._out = None
         self._discarded = None
+        self._filtered = None
+
+        self._quot = misc.quotationMarks | misc.brackets
         
     def read_gold(self, gold_file):
         """Reads the gold file and fills the {page -> category} map from them.
@@ -209,14 +215,18 @@ class NERTrainingCallback(DefaultConllCallback):
         self._out = FileWriter(file_name + '.ner').open()
         if self._keep_discarded:
             self._discarded = FileWriter(file_name + '.discarded').open()
+        if self._keep_filtered:
+            self._filtered = FileWriter(file_name + '.filtered').open()
             
     def fieldStart(self, field):
+#        print "NERTrainingCallback:fieldStart"
         DefaultConllCallback.fieldStart(self, field.lower())
         # We get the category from the title, and also need the words it
         # contains before the last '(' (after which it only contains Wikipedia-
         # specific information, such as disambiguation, etc.) for the link
         # inference trie
         if (self.cc_field.lower() == 'title'):
+            print "PAGE", self.cc_title.encode('utf-8')
             self._title = []
             self._title_pars = self.cc_title.count('(')
             if self._title_pars == 0:
@@ -231,9 +241,14 @@ class NERTrainingCallback(DefaultConllCallback):
             self.first = True
             self._mode = NERTrainingCallback.NO_LINK
             self.tmp = []
+            self.sentence_words = []
     
     def word(self, attributes):
-        """Records the words in a C{SentenceData} object."""
+        """
+        Puts all tokens to self.sentence_words, so that we can do sentence-
+        level checks first.
+        """
+#        print "NERTrainingCallback:word", attributes
         if not self.cc_redirect:
             if self.cc_field.lower() == 'title':
                 if attributes[NERTrainingCallback.RAW] == '(':
@@ -241,7 +256,6 @@ class NERTrainingCallback(DefaultConllCallback):
                 if self._title_pars > 0:
                     self._title.append(attributes[NERTrainingCallback.LEMMA])
             elif self.cc_field.lower() == 'body':
-
                 if len(attributes) >= 5:
                     # Redirect check
                     if self.first:
@@ -250,60 +264,70 @@ class NERTrainingCallback(DefaultConllCallback):
                             self.cc_redirect = True
                             return
 
-                    # State machine part
-                    #print "ATTR", attributes
-                    if self._mode == NERTrainingCallback.NER_LINK:
-                        if attributes[1] == 'B-link':
-#                            print "NER -> NER"
-                            self.__add_tmp()
-                            self.__start_link(attributes)
-                        elif attributes[1] == 'I-link':
-#                            print "NER -> sNER"
-                            self._sent.bi = 'I'
-                        else:
-#                            print "NER -> NO/NNP"
-                            self.__add_tmp()
-                            self._sent.ner_type = 0
-                            if self.__is_NNP(attributes):
-                                self._mode = NERTrainingCallback.NNP_LINK
-                            else:
-                                self._mode = NERTrainingCallback.NO_LINK
-                    elif self._mode == NERTrainingCallback.NNP_LINK:
-                        if attributes[1] == 'B-link':
-#                            print "NNP -> NER"
-                            self.__add_tmp()
-                            self.__start_link(attributes)
-                        elif not self.__is_NNP(attributes):
-#                            print "NNP -> NO"
-                            self.__add_tmp()
-                            self._mode = NERTrainingCallback.NO_LINK
-                            self._sent.ner_type = 0
-#                        else:
-#                            print "NNP -> NNP"
-                    elif self._mode == NERTrainingCallback.NO_LINK:
-                        if attributes[1] == 'B-link':
-#                            print "NO -> NER"
-                            self.__add_tmp()
-                            self.__start_link(attributes)
-                        elif self.__is_NNP(attributes):
-#                            print "NO -> NNP"
-                            self.__add_tmp()
-                            self._mode = NERTrainingCallback.NNP_LINK
-#                        else:
-#                            print "NO -> NO"
-
-                    self.tmp.append(attributes)
+                    # Real work part
+                    self.sentence_words.append(attributes)
                 else:
                     # TODO: logging
                     sys.stderr.write("Illegal word record [{0}] in page {1}\n".format(
                         u','.join(attributes).encode('utf-8'),
                         self.cc_title.encode('utf-8')))
-    
+
+    def word2(self, attributes):
+        """
+        Records the words in a C{SentenceData} object. Called by sentenceEnd
+        in the same way word would be called by the reader.
+        """
+#        print "NERTrainingCallback:word2", attributes
+        sys.stdout.flush()
+        #print "ATTR", attributes
+        if self._mode == NERTrainingCallback.NER_LINK:
+            if attributes[1] == 'B-link':
+#                            print "NER -> NER"
+                self.__add_tmp()
+                self.__start_link(attributes)
+            elif attributes[1] == 'I-link':
+#                            print "NER -> sNER"
+                self._sent.bi = 'I'
+            else:
+#                            print "NER -> NO/NNP"
+                self.__add_tmp()
+                self._sent.ner_type = 0
+                if self.__is_NNP(attributes):
+                    self._mode = NERTrainingCallback.NNP_LINK
+                else:
+                    self._mode = NERTrainingCallback.NO_LINK
+        elif self._mode == NERTrainingCallback.NNP_LINK:
+            if attributes[1] == 'B-link':
+#                            print "NNP -> NER"
+                self.__add_tmp()
+                self.__start_link(attributes)
+            elif not self.__is_NNP(attributes):
+#                            print "NNP -> NO"
+                self.__add_tmp()
+                self._mode = NERTrainingCallback.NO_LINK
+                self._sent.ner_type = 0
+#                        else:
+#                            print "NNP -> NNP"
+        elif self._mode == NERTrainingCallback.NO_LINK:
+            if attributes[1] == 'B-link':
+#                            print "NO -> NER"
+                self.__add_tmp()
+                self.__start_link(attributes)
+            elif self.__is_NNP(attributes):
+#                            print "NO -> NNP"
+                self.__add_tmp()
+                self._mode = NERTrainingCallback.NNP_LINK
+#                        else:
+#                            print "NO -> NO"
+
+        self.tmp.append(attributes)
+
     def __add_tmp(self):
+#        print "NERTrainingCallback:__add_tmp"
         if len(self.tmp) == 0:
             return
 
-        print "TMP", self.tmp, self._sent.ner_type
+#        print "TMP", self.tmp, self._sent.ner_type
         """Adds the temporary chunk to the output sentence."""
         if self._mode == NERTrainingCallback.NER_LINK:
             for i, attributes in enumerate(self.tmp):
@@ -317,6 +341,8 @@ class NERTrainingCallback(DefaultConllCallback):
             for attributes in self.tmp:
                 self._sent.append(attributes)
         elif self._mode == NERTrainingCallback.NNP_LINK:
+#            print "NNP", self.tmp, self._sent.ner_type, len(self.tmp)
+            sys.stdout.flush()
 #            print "TRIE", self._trie.paths
             sentence_start_non_nnp = False
             for partition in all_partitions(self.tmp):
@@ -358,19 +384,23 @@ class NERTrainingCallback(DefaultConllCallback):
 
     def __has_noun(self, chunk):
         """Returns @c True, if there is at least one noun in chunk."""
+#        print "NERTrainingCallback:__has_noun"
         return any(attr[NERTrainingCallback.POS].startswith(u'NOUN')
                    for attr in chunk)
 
     def __get_gold(self, link):
+#        print "NERTrainingCallback:__get_gold"
         return self._gold_map.get(link.lower(), 0)
 
     def __add_redirects(self, link, category):
         """Adds the redirects of a link to the trie."""
+#        print "NERTrainingCallback:__add_redirects"
         for redirect in self._redirect_map.get(link, []):
             self._trie.add_title(redirect, category)
 
     def __start_link(self, attributes):
         """Temporary, to refactor."""
+#        print "NERTrainingCallback:__start_link"
         self._sent.ner_type = self.__get_gold(attributes[NERTrainingCallback.LINK])
         self._mode = NERTrainingCallback.NER_LINK
         self._sent.bi = 'B'
@@ -387,7 +417,7 @@ class NERTrainingCallback(DefaultConllCallback):
  #           print "LINK LOST", attributes
             # Lowercase links point to common nouns
             if attributes[NERTrainingCallback.RAW][0].islower():
-                print "HAHA", attributes
+#                print "HAHA", attributes
                 self._sent.ner_type = 0
             else:
                 self._sent.ner_type = 'UNK'
@@ -396,6 +426,7 @@ class NERTrainingCallback(DefaultConllCallback):
     def __is_NNP(self, attributes):
         """Decides if word is NNP. Also, if a title-cased word occurs in the
         title as well, sets the ner type to the current page's type (if any)."""
+#        print "NERTrainingCallback:fieldStart"
         if attributes[NERTrainingCallback.LEMMA].isdigit():
             if (attributes[NERTrainingCallback.LEMMA] in self._trie.words and
                 'CAS' in attributes[NERTrainingCallback.POS]):
@@ -415,19 +446,47 @@ class NERTrainingCallback(DefaultConllCallback):
     def sentenceEnd(self):
         """If the sentence contains links only to entities whose types are
         known, it is written to the output file."""
+#        print "NERTrainingCallback:sentenceEnd"
+        sys.stdout.flush()
         if self.cc_field.lower() == 'body':
-#            print "SEND"
-            self.__add_tmp()
-            if self._sent.links_lost == 0:
-#                print "NO LOST FOUND"
-                self._sent.write(self._out)
-            elif self._keep_discarded:
-#                print "DISCARDED + LOST FOUND"
-                self._sent.write(self._discarded)
-            self._sent.next_sentence()
-    
+            if len(self.sentence_words) > 1:
+                if self.__ends_in_punct():
+                    for attributes in self.sentence_words:
+                        self.word2(attributes)
+        #            print "SEND"
+                    self.__add_tmp()
+                    if self._sent.links_lost == 0:
+        #                print "NO LOST FOUND"
+                        self._sent.write(self._out)
+                    elif self._keep_discarded:
+        #                print "DISCARDED + LOST FOUND"
+                        self._sent.write(self._discarded)
+                    self._sent.next_sentence()
+                elif self._keep_filtered:
+    #                print "FILTERED"
+                    for attributes in self.sentence_words:
+                        self._filtered.write(u"\t".join(attributes) + u"\n")
+                    self._filtered.write(u"\n")
+            self.sentence_words = []
+
+    def __ends_in_punct(self):
+        """
+        Returns @c True, if the current sentence is valid, i.e. ends with one
+        of the usual sentence-ending punctuation marks.
+        """
+        for token in reversed(self.sentence_words):
+            if (token[NERTrainingCallback.LEMMA] in self._punct or 
+                token[NERTrainingCallback.LEMMA][-1] == u'.'):
+                return True
+            elif token[NERTrainingCallback.LEMMA] in self._quot:
+                continue
+            else:
+                break
+        return False
+
     def fileEnd(self):
         """Closes the output file."""
+#        print "NERTrainingCallback:fileEnd"
         if self._sent.num_train > 0:
             sys.stderr.write("Written {0} sentences out of {1}, with avg length = {2} for file {3}.\n".format(
                     self._sent.num_train, self._sent.num_sentences,
@@ -440,6 +499,9 @@ class NERTrainingCallback(DefaultConllCallback):
         if self._discarded is not None:
             self._discarded.close()
             self._discarded = None
+        if self._filtered is not None:
+            self._filtered.close()
+            self._filtered = None
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -452,6 +514,9 @@ if __name__ == '__main__':
     option_parser.add_option("-d", "--discarded", dest="discarded",
             action="store_true", default=False,
             help="also generate .discarded files.")
+    option_parser.add_option("-f", "--filtered", dest="filtered",
+            action="store_true", default=False,
+            help="also generate .filtered files.")
     options, args = option_parser.parse_args()
 
     if len(args) < 3:
@@ -468,7 +533,7 @@ if __name__ == '__main__':
 
     lt = LanguageTools(args[0], options.language)
     trie = Tries(lt)
-    ntc = NERTrainingCallback(trie, options.discarded)
+    ntc = NERTrainingCallback(trie, options.discarded, options.filtered)
     ntc.read_gold(args[1])
     if options.redirects is not None:
         ntc.read_redirects(options.redirects)
